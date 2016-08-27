53452fbb071a4ff8cb77dcc074f21ff039a6be3d
halityurttas <halityurttas@gmail.com>
2016-08-27 20:08:48 +0300

    Clear usings

--- RepositoryPattern.Core/DomainObjecs/IEntity.cs
+++ RepositoryPattern.Core/DomainObjecs/IEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RepositoryPattern.Core.DomainObjecs
+﻿namespace RepositoryPattern.Core.DomainObjecs
 {
     public interface IEntity<TKey>
     {
